d6abe0b9e34d7cb1e6f1a95bc50defe8ae90e04d
sergio <sergio@MacBook-Pro-de-sergio.local>
2020-12-16 18:00:51 -0600

    Add signin

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -31,10 +31,10 @@ const InfoSection = ( {lightBg, id, imgStart, topLine, lightText, headLine, dark
                                     <Button 
                                     to='home'
                                     smooth={true}
-                                    duration={500}
-                                    spy={true}
-                                    exact='true'
-                                    offset={-80}
+                                    duration={500} 
+                                    spy={true} 
+                                    exact='true' 
+                                    offset={-80} 
                                     primary={primary ? 1 : 0}
                                     dark={dark ? 1 : 0}
                                     
